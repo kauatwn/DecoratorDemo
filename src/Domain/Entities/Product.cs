0011b8f0e179d9dab7514d9de837a961609e6c49
kauatwn <kaualopescontato@gmail.com>
2025-05-06 11:49:46 -0300

    Adiciona classe Product

---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Product(int id, string name, decimal price)
+{
+    public int Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+}
