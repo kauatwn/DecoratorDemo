01a240cccd65c5f0b2a00b8c91096e7a3443b71e
kauatwn <kaualopescontato@gmail.com>
2025-05-06 11:50:18 -0300

    Adiciona interfaces ICacheService e IProductRepository

---
+++ src/Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface IProductRepository
+{
+    void Add(Product product);
+    Task<IReadOnlyCollection<Product>> GetAllAsync();
+}
