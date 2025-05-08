cac13b61cf1d1d9b75a0a7743e2a29bb57e9f244
kauatwn <kaualopescontato@gmail.com>
2025-05-07 21:27:19 -0300

    Adiciona classe ProductRepository

---
+++ src/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly List<Product> _products =
+    [
+        new(1, "Notebook", 5000),
+        new(2, "Smartphone", 3000),
+        new(3, "Tablet", 2000),
+        new(4, "Monitor", 1500),
+        new(5, "Teclado", 200),
+        new(6, "Mouse", 100),
+        new(7, "Impressora", 800),
+        new(8, "Cadeira", 1200),
+        new(9, "Mesa", 1500),
+        new(10, "Fone de Ouvido", 300)
+    ];
+
+    public void Add(Product product) => _products.Add(product);
+
+    public async Task<IReadOnlyCollection<Product>> GetAllAsync()
+    {
+        Console.WriteLine("--> Consultando banco de dados...");
+        await Task.Delay(TimeSpan.FromSeconds(2));
+
+        return _products.AsReadOnly();
+    }
+}
