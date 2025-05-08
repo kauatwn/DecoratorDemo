40bce8cb0bc52e4aeb4b0c24713d5c767df4586a
kauatwn <kaualopescontato@gmail.com>
2025-05-07 21:31:55 -0300

    Adiciona classe CachedProductRepository

---
+++ src/Infrastructure/Decorators/CachedProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Decorators.Bases;
+
+namespace Infrastructure.Decorators;
+
+public class CachedProductRepository(IProductRepository decorated, ICacheService cache)
+    : ProductRepositoryDecorator(decorated)
+{
+    private const string AllProductsCacheKey = "all-products";
+
+    public override void Add(Product product)
+    {
+        base.Add(product);
+
+        cache.Remove(AllProductsCacheKey);
+        Console.WriteLine("--> Cache de produtos removido devido à adição de um novo produto.");
+    }
+
+    public override async Task<IReadOnlyCollection<Product>> GetAllAsync()
+    {
+        if (cache.Get<IReadOnlyCollection<Product>>(AllProductsCacheKey) is { } cachedProducts)
+        {
+            Console.WriteLine("--> Retornando lista de produtos do cache...");
+
+            return cachedProducts;
+        }
+
+        IReadOnlyCollection<Product> products = await base.GetAllAsync();
+        cache.Set(AllProductsCacheKey, products, TimeSpan.FromSeconds(30));
+
+        return products;
+    }
+}
