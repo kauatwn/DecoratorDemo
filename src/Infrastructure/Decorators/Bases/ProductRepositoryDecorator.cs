9cc8d6f7f3cd9d92c3e74351c6bcaea64cd9d2fe
kauatwn <kaualopescontato@gmail.com>
2025-05-07 21:30:16 -0300

    Adiciona classe ProductRepositoryDecorator

---
+++ src/Infrastructure/Decorators/Bases/ProductRepositoryDecorator.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Decorators.Bases;
+
+public abstract class ProductRepositoryDecorator(IProductRepository decorated) : IProductRepository
+{
+    public virtual void Add(Product product) => decorated.Add(product);
+
+    public virtual async Task<IReadOnlyCollection<Product>> GetAllAsync() => await decorated.GetAllAsync();
+}
