a4e832a1f98c7ed92c455d7010f998d00b3a5ed3
kauatwn <kaualopescontato@gmail.com>
2025-05-07 21:33:33 -0300

    Adiciona registro de dependências para Infrastructure

---
+++ src/Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Decorators;
+using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        AddCaching(services);
+        AddRepositories(services);
+    }
+
+    private static void AddCaching(IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<ICacheService, MemoryCacheService>();
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddSingleton<IProductRepository>(provider =>
+        {
+            var repository = new ProductRepository();
+            var cache = provider.GetRequiredService<ICacheService>();
+
+            return new CachedProductRepository(repository, cache);
+        });
+    }
+}
