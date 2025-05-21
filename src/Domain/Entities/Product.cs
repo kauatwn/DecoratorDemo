aa18da1fc37850b0bd5e48da05f8a428bc055ec3
kauatwn <kaualopescontato@gmail.com>
2025-05-21 20:27:40 -0300

    Altera tipo de Price para double e torna propriedades somente leitura

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,8 +1,8 @@
 ﻿namespace Domain.Entities;
 
-public class Product(int id, string name, decimal price)
+public class Product(int id, string name, double price)
 {
-    public int Id { get; set; } = id;
-    public string Name { get; set; } = name;
-    public decimal Price { get; set; } = price;
+    public int Id { get; } = id;
+    public string Name { get; } = name;
+    public double Price { get; } = price;
 }
