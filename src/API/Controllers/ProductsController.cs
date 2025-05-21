a371365458301bb2485319df1f67fe346f60d59b
kauatwn <kaualopescontato@gmail.com>
2025-05-21 20:29:34 -0300

    Adiciona classe ProductsController

---
+++ src/API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType<IEnumerable<Product>>(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts(IGetAllProductsUseCase useCase) =>
+        Ok(await useCase.Execute());
+
+    [HttpPost]
+    [ProducesResponseType<Product>(StatusCodes.Status201Created)]
+    public ActionResult<Product> AddProduct(IAddProductUseCase useCase, Product product)
+    {
+        useCase.Execute(product);
+
+        return CreatedAtAction(nameof(GetAllProducts), null, product);
+    }
+}
