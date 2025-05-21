084600a6ba6c2f64def68c538d25c7603a345aaf
kauatwn <kaualopescontato@gmail.com>
2025-05-21 20:23:33 -0300

    Adiciona classe AddProductUseCase

---
+++ src/Application/UseCases/AddProductUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.UseCases;
+
+public class AddProductUseCase(IProductRepository repository) : IAddProductUseCase
+{
+    public void Execute(Product product) => repository.Add(product);
+}
