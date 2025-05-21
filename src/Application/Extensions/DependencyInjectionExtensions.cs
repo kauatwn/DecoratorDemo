e63600606d8759b52b0a1a40510757e67b7331ff
kauatwn <kaualopescontato@gmail.com>
2025-05-21 20:24:38 -0300

    Adiciona registro de dependências para Application

---
+++ src/Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstractions;
+using Application.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddApplication(this IServiceCollection services) => AddUseCases(services);
+
+    private static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IGetAllProductsUseCase, GetAllProductsUseCase>();
+        services.AddScoped<IAddProductUseCase, AddProductUseCase>();
+    }
+}
