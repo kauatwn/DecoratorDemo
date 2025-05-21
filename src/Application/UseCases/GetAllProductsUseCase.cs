6c64817f6c9ef8c84b6d16437cec5ef37eadf591
kauatwn <kaualopescontato@gmail.com>
2025-05-21 20:23:42 -0300

    Adiciona classe GetAllProductsUseCase

---
+++ src/Application/UseCases/GetAllProductsUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Application.Abstractions;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.UseCases;
+
+public class GetAllProductsUseCase(IProductRepository repository) : IGetAllProductsUseCase
+{
+    public async Task<IEnumerable<Product>> Execute() => await repository.GetAllAsync();
+}
