de984e7e3de2cebcda9ec5890375e6ae0afee2c9
kauatwn <kaualopescontato@gmail.com>
2025-06-05 23:04:20 -0300

    Altera tipo de Price para decimal

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -1,8 +1,8 @@
 ﻿namespace Domain.Entities;
 
-public class Product(int id, string name, double price)
+public class Product(int id, string name, decimal price)
 {
     public int Id { get; } = id;
     public string Name { get; } = name;
-    public double Price { get; } = price;
+    public decimal Price { get; } = price;
 }
