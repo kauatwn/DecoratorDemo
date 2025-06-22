642d13d2c388516fca15e6c290e6a0c4c8e48331
kauatwn <kaualopescontato@gmail.com>
2025-06-22 18:50:30 -0300

    Refatora ProductRepository para usar InMemoryAppDbContext

--- src/DecoratorDemo.Infrastructure/Repositories/ProductRepository.cs
+++ src/DecoratorDemo.Infrastructure/Repositories/ProductRepository.cs
@@ -1,31 +1,26 @@
 ﻿using DecoratorDemo.Domain.Entities;
 using DecoratorDemo.Domain.Interfaces.Repositories;
+using DecoratorDemo.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace DecoratorDemo.Infrastructure.Repositories;
 
-public class ProductRepository : IProductRepository
+public class ProductRepository(InMemoryAppDbContext context, bool simulateLatency = true) : IProductRepository
 {
-    private readonly List<Product> _products =
-    [
-        new(id: 1, name: "Notebook", price: 5_000m),
-        new(id: 2, name: "Smartphone", price: 3_000m),
-        new(id: 3, name: "Tablet", price: 2_000m),
-        new(id: 4, name: "Monitor", price: 1_500m),
-        new(id: 5, name: "Teclado", price: 200m),
-        new(id: 6, name: "Mouse", price: 100m),
-        new(id: 7, name: "Impressora", price: 800m),
-        new(id: 8, name: "Cadeira", price: 1_200m),
-        new(id: 9, name: "Mesa", price: 1_500),
-        new(id: 10, name: "Fone de Ouvido", price: 300m)
-    ];
-
-    public void Add(Product product) => _products.Add(product);
+    public void Add(Product product)
+    {
+        context.Products.Add(product);
+        context.SaveChanges();
+    }
 
     public async Task<IReadOnlyCollection<Product>> GetAllAsync()
     {
-        Console.WriteLine("--> Consultando banco de dados...");
-        await Task.Delay(TimeSpan.FromSeconds(2));
+        if (simulateLatency)
+        {
+            Console.WriteLine("--> Simulando latência de banco real...");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+        }
 
-        return _products.AsReadOnly();
+        return await context.Products.AsNoTracking().ToListAsync();
     }
 }
