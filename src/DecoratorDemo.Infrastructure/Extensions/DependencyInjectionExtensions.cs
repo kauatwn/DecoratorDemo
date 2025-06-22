b27054dce4db99cdb56171ef6e36648fc487d644
kauatwn <kaualopescontato@gmail.com>
2025-06-22 18:54:54 -0300

    Registra InMemoryAppDbContext e ajusta DI do ProductRepository para usar contexto

--- src/DecoratorDemo.Infrastructure/Extensions/DependencyInjectionExtensions.cs
+++ src/DecoratorDemo.Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using DecoratorDemo.Application.Interfaces.Services;
 using DecoratorDemo.Domain.Interfaces.Repositories;
+using DecoratorDemo.Infrastructure.Context;
 using DecoratorDemo.Infrastructure.Decorators;
 using DecoratorDemo.Infrastructure.Repositories;
 using DecoratorDemo.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DecoratorDemo.Infrastructure.Extensions;
@@ -11,8 +13,14 @@ public static class DependencyInjectionExtensions
 {
     public static void AddInfrastructure(this IServiceCollection services)
     {
+        AddInMemoryDbContext(services);
         AddCaching(services);
         AddRepositories(services);
+    }
+
+    private static void AddInMemoryDbContext(IServiceCollection services)
+    {
+        services.AddDbContext<InMemoryAppDbContext>(options => options.UseInMemoryDatabase("ProductsDb"));
     }
 
     private static void AddCaching(IServiceCollection services)
@@ -23,11 +31,12 @@ public static class DependencyInjectionExtensions
 
     private static void AddRepositories(IServiceCollection services)
     {
-        services.AddSingleton<IProductRepository>(provider =>
+        services.AddScoped<IProductRepository>(provider =>
         {
-            var repository = new ProductRepository();
-            var cache = provider.GetRequiredService<ICacheService>();
+            var context = provider.GetRequiredService<InMemoryAppDbContext>();
+            ProductRepository repository = new(context);
 
+            var cache = provider.GetRequiredService<ICacheService>();
             return new CachedProductRepository(repository, cache);
         });
     }
